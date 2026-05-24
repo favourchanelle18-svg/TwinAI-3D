c177c7851a99a454105fb27b34e30a45f4ec7b40
favourchanelle18-svg <favourchanelle18@gmail.com>
2026-05-24 16:37:35 +0400

    Include external CSS file for styling
    Add CSS styling to Streamlit app

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 import streamlit as st
 from PIL import Image
+with open("styles.css") as f:
+    st.markdown(
+        f"<style>{f.read()}</style>",
+        unsafe_allow_html=True
+    )
 
 st.set_page_config(
     page_title="TwinAI 3D",
